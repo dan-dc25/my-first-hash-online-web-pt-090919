3f7c073cbfa0afc1aef37ac1cf50383f7c7cae4d
Daniel Alves de Carvalho <danielcarvalhovr@gmail.com>
2019-09-28 22:22:08 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -34,5 +34,5 @@ def adding
   # add 4 gun powder to the shipping_manifest hash below
   shipping_manifest["gun powder"] = 4
   # return the shipping_manifest hash below
-  puts shipping_manifest
+  return shipping_manifest
 end
